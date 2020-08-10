504532aaac06e618c7fd1eefc64c10013a8ed778
gaot <18612792331@163.com>
2020-08-10 09:30:15 +0800

    排行榜 关键词搜索

--- src/main/java/com/anyulu/wechat/rest/IndexResource.java
+++ src/main/java/com/anyulu/wechat/rest/IndexResource.java
@@ -4,6 +4,10 @@ import com.anyulu.wechat.domain.DataYulu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Random;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/api")
@@ -20,6 +25,11 @@ public class IndexResource {
 
     @Autowired
     private MongoTemplate mongo;
+
+    @GetMapping("/success")
+    public String success() {
+        return "success";
+    }
 
     @GetMapping("/SweetNothings")
     public DataYulu getRandowData() {
@@ -47,10 +57,29 @@ public class IndexResource {
         return yulu.getCaiCount();
     }
 
-    public List<DataYulu> getTop10() {
+    @GetMapping("/rank")
+    public Page<DataYulu> getRank(@RequestParam("pageNum") Integer pageNum, @RequestParam("pageSize") Integer pageSize) {
         log.info("获取语录排行榜");
+        PageRequest pageRequest = PageRequest.of(pageNum - 1, pageSize);
+        Query query = new Query();
+        query.with(Sort.by(
+                Sort.Order.desc("zanCount")
+        ));
+        long total = mongo.count(query, DataYulu.class);
+        List<DataYulu> list = mongo.find(query.with(pageRequest), DataYulu.class);
+        Page<DataYulu> page = new PageImpl(list, pageRequest, total);
 
-        return null;
+        return page;
+    }
+
+    @GetMapping("/search/{keyword}")
+    public List<DataYulu> search(@PathVariable String keyword) {
+        log.info("根据关键词搜索 :{}",keyword);
+        Query query = new Query();
+        Pattern pattern=Pattern.compile("^.*"+keyword+".*$", Pattern.CASE_INSENSITIVE);
+        query.addCriteria(Criteria.where("text").regex(pattern));
+        List<DataYulu> dataYulus = mongo.find(query, DataYulu.class);
+        return dataYulus;
     }
 
 
